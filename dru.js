078563ddbd701d6874edfa0c8a7dfc158b5eab44
dru18 <dru41821@gmail.com>
2019-06-10 16:56:52 +0530

    modified:   dru.js

--- dru.js
+++ dru.js
@@ -13,6 +13,19 @@ const M = new Mastodon({
     api_url: 'https://botsin.space/api/v1/', // optional, defaults to https://mastodon.social/api/v1/
   });
 
+  function notify(){
+    M.get('notifications', {type: 'follow', limit: '3'}, (error, data) => {
+        if(error){console.log(error);}
+        else{
+        console.log(`*****notifications***** \n${data[0].account.username} (${data[0].account.acct}) - ${data[0].type}`);
+        console.log(`${data[1].account.username} (${data[1].account.acct}) - ${data[1].type}`);
+        console.log(`${data[2].account.username} (${data[2].account.acct}) - ${data[2].type}`);
+      }
+        //else{console.log(data); }
+    });
+  }
+  notify();
+
 
 function followBack(){
   M.get('timelines/home', {limit: '1'}, (error, data) => {
@@ -48,13 +61,35 @@ function followBack(){
 //  followBack();
 //setInterval(getUpdate, 1000);
 
-function notify(){
-  M.get('notifications', {}, (error, data) => {
-      if(error){console.log(error);}
-      else{console.log(`*****notifications***** \n ${data[0].account.username} (${data[0].account.acct}) - ${data[0].type} - ${data[0].status.content}`);}
+
+function toot(txt){
+  M.post('statuses', {status: txt, visibility: 'public', spoiler_text: 'Jai Shree Ram'}, (error, data) => {
+    if(error){console.log(error)}
+    else{console.log(data.content)}
   });
 }
-notify();
+//toot('The meaning of life is: "Remember what you have ACHIEVED."');
+
+
+function replyFev(){
+  M.get('notifications', {}, (error, data) => {
+    const stsType = data[0].type;
+    const usrName = data[0].account.username;
+    const usrAccount = data[0].account.acct;
+    const stsContent = data[0].status.content;
+
+    if(error){console.log(error);}
+    
+    else{
+      console.log(`*****notifications***** \n ${usrName} ${usrAccount} - ${stsType} - ${stsContent}`);
+      //M.post('statuses', {status: `@${data[0].account.acct} Jai Shree Ram`, visibility: 'direct'}, (error, resp) => {
+      M.post(`statuses/${data[0].status.id}/favourite`);  
+      //console.log(`Replied ${resp.content} to ${resp.account.acct}`);
+      }
+  });
+}
+//replyFev();
+
 
 function nClear(){
   M.post('notifications/clear', {}, (error, data) => {
@@ -66,11 +101,4 @@ function nClear(){
   });
 }
 //nClear();
-
-function toot(txt){
-  M.post('statuses', {status: txt, visibility: 'public', spoiler_text: 'Jai Shree Ram'}, (error, data) => {
-    if(error){console.log(error)}
-    else{console.log(data.content)}
-  });
-}
-//toot('The meaning of life is: "Remember what you have ACHIEVED."');
+//notify();
